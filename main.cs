4760a51573c0e4ffdb4c3c0c7e18b315d0f2f55c
Johanna Liu <99354992+Johanna-liu16@users.noreply.github.com>
2022-04-27 17:21:54 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,30 @@
+// Created by: Johanna Liu
+// Created on: Mar 2022
+//
+// This program does basic math
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // This function states user's address input. 
+        int userInteger;
+
+        //Input 
+        Console.WriteLine("This program identifies integers.");
+        Console.WriteLine("");
+        Console.Write("Enter an integer: ");
+        userInteger = Convert.ToInt32(Console.ReadLine());
+
+        //Process
+        if (userInteger >= 0) {
+            Console.WriteLine("Integer is a positive number.");
+        } else {
+            Console.WriteLine("Integer is a negative number.");
+        }
+
+        Console.WriteLine("\nDone.");
+    }
+}
